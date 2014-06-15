370ea45f024e39419da6368f845b75e058fc8308
nikolay-radkov <nikolay.kirilov.radkov@gmail.com>
2014-06-15 15:33:22 +0300

    Fix diagonal ninja moving

--- ninja.js
+++ ninja.js
@@ -4,12 +4,12 @@ function Ninja(x, y, width, height, images) {
     this.width = width;
     this.height = height;
     this.images = images;
-    
+
     this.speedX = 0;
     this.speedY = 0;
     this.isAlive = true;
     this.type = "ninja";
-    
+
     this.currentFrame = 0;
     this.framesPerSprite = 15;
     this.currentSprite = 0;
@@ -18,10 +18,10 @@ function Ninja(x, y, width, height, images) {
     this.draw = function (context) {
         context.drawImage(images[this.currentSprite], this.x, this.y, this.width, this.height);
     }
-    
+
     this.collideWith = function (otherObject) {
-        if (otherObject.type === "homework"){
-            if (isColliding(this, otherObject)){
+        if (otherObject.type === "homework") {
+            if (isColliding(this, otherObject)) {
                 this.isAlive = false;
             }
         }
@@ -55,53 +55,55 @@ function Ninja(x, y, width, height, images) {
         if (controls.pressedKeys[40]) {
             this.speedY = ninjaSpeed;
 
-            if (hasPressedVerticalArrolKey)
-            {
+            if (hasPressedVerticalArrolKey) {
                 hasPressedVerticalArrolKey = false;
             } else {
                 hasPressedVerticalArrolKey = true;
             }
         }
 
-        if (!(hasPressedHorizontalArrolKey || hasPressedVerticalArrolKey)) {
+        if (!hasPressedHorizontalArrolKey) {
             this.speedX = 0;
+        }
+
+        if (!hasPressedVerticalArrolKey) {
             this.speedY = 0;
         }
     }
 
     this.move = function (leftBorderX, topBorderY, rightBorderX, bottomBorderY) {
         // Check if the ninja is moving
-        if (this.speedX === 0 && this.speedY === 0){
+        if (this.speedX === 0 && this.speedY === 0) {
             return;
         }
-        
+
         // Move the ninja within the given bounds
         var newPosition = {
             x: this.x + this.speedX,
             y: this.y + this.speedY
         };
-        
-        if (newPosition.x < leftBorderX){
+
+        if (newPosition.x < leftBorderX) {
             newPosition.x = leftBorderX;
         }
-        if ( newPosition.y < topBorderY){
+        if (newPosition.y < topBorderY) {
             newPosition.y = topBorderY;
         }
-        if ( newPosition.x + this.width > rightBorderX){
+        if (newPosition.x + this.width > rightBorderX) {
             newPosition.x = rightBorderX - this.width;
         }
-        if ( newPosition.y + this.height > bottomBorderY){
+        if (newPosition.y + this.height > bottomBorderY) {
             newPosition.y = bottomBorderY - this.height;
         }
-        
+
         // Update the ninja sprite
         this.currentFrame++;
-        if (this.currentFrame === this.framesPerSprite){
+        if (this.currentFrame === this.framesPerSprite) {
             ++this.currentSprite;
             this.currentFrame = 0;
         }
-        
-        if ( this.currentSprite === this.numberOfSprites){
+
+        if (this.currentSprite === this.numberOfSprites) {
             this.currentSprite = 0;
         }
 
